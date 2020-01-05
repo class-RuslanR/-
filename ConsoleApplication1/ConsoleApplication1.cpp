643d2470b05c08950c4a46a508bcc8e0dbd97465
Ruslan Rogozhkin <57987419+class-RuslanR@users.noreply.github.com>
2020-01-05 16:44:36 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,108 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+protected:
+	int count, b[5];
+public:
+	virtual void Set(int mass[])
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			b[i] = mass[i];
+		}
+	}
+	void print()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			cout << b[i] << endl;
+		}
+	}
+};
+
+class A_int_ch : public A
+{
+public:
+	void Set(int mass[])
+	{
+		count = 0;
+		for  ( int i = 0; i< 5; i ++)
+		{
+			if (mass[i] % 2 == 0)
+			{
+				b[count] = mass[i];
+				count++;
+			}
+			else 
+			{
+				b[count] = 0;
+				count++;
+			}
+		}
+	}
+	void printCh()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			cout << b[count] << endl;
+		}
+	}
+};
+
+class A_int_nch : public A
+{
+public:
+	void Set(int mass[])
+	{
+		count = 0;
+		for (int i = 0; i < 5; i ++)
+		{
+			if (mass[i] % 2 != 0)
+			{
+				b[count] = mass[i];
+				count++;
+			}
+
+			else 
+			{
+				b[count] = 0;
+				count++;
+			}
+		}
+	}
+	void printNch()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			cout << b[count] << endl;
+		}
+	}
+};
+
+int main()
+{
+	A objA;
+	int mass[5] = { 1,2,3,4,5 };
+	//objA.Set(mass);
+	//objA.print();
+
+	A *pr = &objA;
+	A_int_ch objCh;
+	A_int_nch objNch;
+	pr = &objCh;
+	pr->Set(mass);
+	pr-> print();
+
+	cout << "\n\n";
+
+	pr = &objNch;
+	pr->Set(mass);
+	pr->print();
+
+	return 0;
+}
+
